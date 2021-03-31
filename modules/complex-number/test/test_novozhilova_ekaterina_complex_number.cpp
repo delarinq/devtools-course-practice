cfc08031de6e36bb971dd7c6ad00d5d1d33e2221
delarinq <eks-pliz@ya.ru>
2021-03-31 15:18:36 +0300

    Made requested changes

--- modules/complex-number/test/test_novozhilova_ekaterina_complex_number.cpp
+++ modules/complex-number/test/test_novozhilova_ekaterina_complex_number.cpp
@@ -6,34 +6,42 @@
 
 TEST(Novozhilova_Ekaterina_ComplexNumberTest,
 Created_With_The_Same_Complex_Number_Are_Equal) {
+    // Arrange
     ComplexNumber z(2.0, -4.5);
 
+    // Act
     ComplexNumber z1(z);
     ComplexNumber z2(z);
 
-    EXPECT_EQ(z1.getRe(), z2.getRe());
-    EXPECT_EQ(z1.getIm(), z2.getIm());
+    // Assert
+    ASSERT_DOUBLE_EQ(z1.getRe(), z2.getRe());
+    ASSERT_DOUBLE_EQ(z1.getIm(), z2.getIm());
 }
 
 TEST(Novozhilova_Ekaterina_ComplexNumberTest, Equals_Origin) {
-    ComplexNumber a(-2.3, 0.0);
+    // Arrange
+    ComplexNumber a(-2.3, 4.5);
 
+    // Act
     ComplexNumber b(a);
     ComplexNumber c(b);
 
-    EXPECT_EQ(a.getRe(), c.getRe());
-    EXPECT_EQ(a.getIm(), c.getIm());
+    // Assert
+    ASSERT_DOUBLE_EQ(a.getRe(), c.getRe());
+    ASSERT_DOUBLE_EQ(a.getIm(), c.getIm());
 }
 
 TEST(Novozhilova_Ekaterina_ComplexNumberTest,
 Res_Are_Not_Equal_Then_Numbers_Arent_Too) {
+    // Arrange
     ComplexNumber a(7.8, 5.0);
-    ComplexNumber b(3.0, 5.0);
-
+    ComplexNumber b(a);
     // 0 is not equal, 1 is equal
     int part_equality;
     int full_equality;
 
+    // Act
+    b.setRe(3.4);
     if (a.getRe() != b.getRe()) {
         part_equality = 0;
     } else {
@@ -44,5 +52,7 @@ Res_Are_Not_Equal_Then_Numbers_Arent_Too) {
     } else {
         full_equality = 1;
     }
-    EXPECT_EQ(part_equality, full_equality);
+
+    // Assert
+    ASSERT_EQ(part_equality, full_equality);
 }
