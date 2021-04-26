a8a87f5768e5f91c1409bec9388a24c858859bed
delarinq <eks-pliz@ya.ru>
2021-04-26 15:52:56 +0300

    separated arrange, act and assert blocks in tests

--- modules/modified_stack_minimums/test/test_modified_stack_minimums.cpp
+++ modules/modified_stack_minimums/test/test_modified_stack_minimums.cpp
@@ -6,80 +6,95 @@
 
 TEST(Novozhilova_Ekaterina_modified_stack_Test, testgetSize) {
     ModifiedStack mystack(6);
+
     ASSERT_EQ(6, mystack.getSize());
 }
 
 TEST(Novozhilova_Ekaterina_modified_stack_Test, testConstructor) {
     ModifiedStack mystack1(6);
+    
     ModifiedStack mystack2(mystack1);
+
     ASSERT_EQ(6, mystack2.getSize());
 }
 
 TEST(Novozhilova_Ekaterina_modified_stack_Test, testIsEmptyPositive) {
     ModifiedStack mystack(10);
-    bool res = false;
+
     ASSERT_TRUE(mystack.isEmpty());
 }
 
 TEST(Novozhilova_Ekaterina_modified_stack_Test, testIsFullPositive) {
     ModifiedStack mystack(3);
+
     mystack.push(1);
     mystack.push(2);
     mystack.push(3);
-    bool res = false;
+
     ASSERT_TRUE(mystack.isFull());
 }
 
 TEST(Novozhilova_Ekaterina_modified_stack_Test, testIsEmptyNegative) {
     ModifiedStack mystack(3);
+
     mystack.push(1);
-    bool res = true;
+
     ASSERT_FALSE(mystack.isEmpty());
 }
 
 TEST(Novozhilova_Ekaterina_modified_stack_Test, testIsFullNegative) {
     ModifiedStack mystack(3);
+
     mystack.push(1);
     mystack.push(2);
-    bool res = true;
+
     ASSERT_FALSE(mystack.isFull());
 }
 
 TEST(Novozhilova_Ekaterina_modified_stack_Test, testgetMin) {
     ModifiedStack mystack(4);
+
     mystack.push(1);
     mystack.push(2);
     mystack.push(3);
+
     ASSERT_EQ(1, mystack.getMin());
 }
 
 TEST(Novozhilova_Ekaterina_modified_stack_Test, testgetMin_shuffledOrder) {
     ModifiedStack mystack(4);
+
     mystack.push(3);
     mystack.push(1);
     mystack.push(2);
+
     ASSERT_EQ(1, mystack.getMin());
 }
 
 TEST(Novozhilova_Ekaterina_modified_stack_Test, testGetTop) {
     ModifiedStack mystack(5);
+
     mystack.push(13);
     mystack.push(5);
     mystack.push(26);
+
     ASSERT_EQ(26, mystack.getTop());
 }
 
 TEST(Novozhilova_Ekaterina_modified_stack_Test, testPop) {
     ModifiedStack mystack(4);
+
     mystack.push(13);
     mystack.push(5);
     mystack.push(26);
     mystack.pop();
+
     ASSERT_EQ(5, mystack.getTop());
 }
 
 TEST(Novozhilova_Ekaterina_modified_stack_Test, testCompareEqualPositive) {
     ModifiedStack mystack1(5);
+
     mystack1.push(20);
     mystack1.push(64);
     mystack1.push(12);
@@ -89,11 +104,13 @@ TEST(Novozhilova_Ekaterina_modified_stack_Test, testCompareEqualPositive) {
     if (mystack1 == mystack2) {
         equality = true;
     }
+
     ASSERT_TRUE(equality);
 }
 
 TEST(Novozhilova_Ekaterina_modified_stack_Test, testCompareDifferentPositive) {
     ModifiedStack mystack1(5);
+
     mystack1.push(20);
     mystack1.push(64);
     mystack1.push(12);
@@ -104,11 +121,13 @@ TEST(Novozhilova_Ekaterina_modified_stack_Test, testCompareDifferentPositive) {
     if (mystack1 != mystack2) {
         inequality = true;
     }
+
     ASSERT_TRUE(inequality);
 }
 
 TEST(Novozhilova_Ekaterina_modified_stack_Test, testCompareEqualNegative) {
     ModifiedStack mystack1(5);
+
     mystack1.push(20);
     mystack1.push(64);
     mystack1.push(12);
@@ -119,11 +138,13 @@ TEST(Novozhilova_Ekaterina_modified_stack_Test, testCompareEqualNegative) {
     if (mystack1 == mystack2) {
         equality = true;
     }
+
     ASSERT_FALSE(equality);
 }
 
 TEST(Novozhilova_Ekaterina_modified_stack_Test, testCompareDifferentNegative) {
     ModifiedStack mystack1(5);
+
     mystack1.push(20);
     mystack1.push(64);
     mystack1.push(12);
@@ -133,30 +154,36 @@ TEST(Novozhilova_Ekaterina_modified_stack_Test, testCompareDifferentNegative) {
     if (mystack1 != mystack2) {
         inequality = true;
     }
+
     ASSERT_FALSE(inequality);
 }
 
 TEST(Novozhilova_Ekaterina_modified_stack_Test, testGetLast) {
     ModifiedStack mystack(5);
+
     mystack.push(20);
     mystack.push(64);
     mystack.push(23);
     mystack.push(34);
+
     ASSERT_EQ(3, mystack.getLast());
 }
 
 TEST(Novozhilova_Ekaterina_modified_stack_Test, testPushInFullStack) {
     ModifiedStack mystack(4);
+
     mystack.push(20);
     mystack.push(64);
     mystack.push(23);
     mystack.push(34);
+
     ASSERT_ANY_THROW(mystack.push(56));
 }
 
 TEST(Novozhilova_Ekaterina_modified_stack_Test, testEqual) {
     ModifiedStack mystack1(4);
     ModifiedStack mystack2(7);
+
     mystack1.push(4);
     mystack1.push(75);
     mystack2 = mystack1;
@@ -164,6 +191,7 @@ TEST(Novozhilova_Ekaterina_modified_stack_Test, testEqual) {
     if (mystack2 == mystack1) {
         equality = true;
     }
+
     ASSERT_TRUE(equality);
 }
 
@@ -171,11 +199,13 @@ TEST(Novozhilova_Ekaterina_modified_stack_Test,
 testSizesAfterEquatingAreTheSame) {
     ModifiedStack mystack1(16);
     ModifiedStack mystack2(7);
+
     mystack1 = mystack2;
     bool equality = false;
     if (mystack1.getSize() == mystack2.getSize()) {
         equality = true;
     }
+
     ASSERT_TRUE(equality);
 }
 
@@ -183,6 +213,7 @@ TEST(Novozhilova_Ekaterina_modified_stack_Test,
 testLastIsTheSameAfterEquating) {
     ModifiedStack mystack1(58);
     ModifiedStack mystack2(63);
+
     mystack1.push(12);
     mystack1.push(14);
     mystack1.push(3);
@@ -194,6 +225,7 @@ testLastIsTheSameAfterEquating) {
     if (mystack1.getLast() == mystack2.getLast()) {
         equality = true;
     }
+
     ASSERT_TRUE(equality);
 }
 
@@ -201,6 +233,7 @@ TEST(Novozhilova_Ekaterina_modified_stack_Test,
 testIfSizesAreDiffThenNotEqual) {
     ModifiedStack mystack1(16);
     ModifiedStack mystack2(6);
+
     ASSERT_FALSE(mystack1 == mystack2);
 }
 
@@ -208,20 +241,24 @@ TEST(Novozhilova_Ekaterina_modified_stack_Test,
 testIfSizesAreDiffThenNotEqual_2) {
     ModifiedStack mystack1(13);
     ModifiedStack mystack2(3);
+
     ASSERT_TRUE(mystack1 != mystack2);
 }
 
 TEST(Novozhilova_Ekaterina_modified_stack_Test,
 testSelfAssignmentAndAssignmet) {
     ModifiedStack mystack1(34);
-    mystack1 = mystack1;
     ModifiedStack mystack2(5);
+
+    mystack1 = mystack1;
     mystack2 = mystack1;
+
     ASSERT_TRUE(mystack1 == mystack2);
 }
 
 TEST(Novozhilova_Ekaterina_modified_stack_Test, testLoopForPushandGetLast) {
     ModifiedStack mystack(5);
+
     for (int i = 0; i < 5; i++) {
         mystack.push(i);
     }
